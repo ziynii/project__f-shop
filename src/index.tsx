c154b821144b43f5608bbe72d40fe1be20ce01f4
Jiyeon <yeeoni_@naver.com>
2022-02-28 18:59:25 +0900

    ✨ Add react-query library

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
 
+const client = new QueryClient();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<QueryClientProvider client={client}>
+			<App />
+		</QueryClientProvider>
+	</React.StrictMode>,
+	document.getElementById('root'),
 );
