83d1ac8d795150ea4f357ea001555f8eed8531c4
Jiyeon <yeeoni_@naver.com>
2022-03-02 23:30:49 +0900

    🚧 Add back button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { goBackState } from '../atom';
 
 const Header = () => {
 	const navigate = useNavigate();
+	const [isGoBack, setIsGoBack] = useRecoilState(goBackState);
+
+	const goBackLocate = () => {
+		setIsGoBack(false);
+		navigate(-1);
+	};
+
+	console.log(isGoBack);
 
 	return (
 		<header className="header">
@@ -10,9 +20,17 @@ const Header = () => {
 				<div className="row">
 					<div className="col-sm-4">
 						<div className="header-wrapper">
-							<button className="header-menu-button sm-only" type="button">
-								<i className="fa-solid fa-bars" />
-							</button>
+							{isGoBack === false ? (
+								<button className="header-menu-button sm-only" type="button">
+									<i className="fa-solid fa-bars" />
+								</button>
+							) : null}
+
+							{isGoBack === true ? (
+								<button className="header-back-button sm-only" type="button" onClick={goBackLocate}>
+									<i className="fa-solid fa-arrow-left"></i>
+								</button>
+							) : null}
 
 							<div className="header-logo" onClick={() => navigate('/')}>
 								<img src="/logo.svg" alt="로고" />
