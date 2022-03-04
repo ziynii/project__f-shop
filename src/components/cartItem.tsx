3f29785ea8a35bc9184254fc78e8f0ebe6590b3e
Jiyeon <yeeoni_@naver.com>
2022-03-04 20:19:49 +0900

    🚧 Add a function to delete the product when Remove button click

--- src/components/cartItem.tsx
+++ src/components/cartItem.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
 import { IProduct } from '../api';
+import { cartItemsState } from '../atom';
 
 interface ICartItemProps {
 	item: IProduct;
 }
 
 const CartItem = ({ item }: ICartItemProps) => {
+	const [cartItems, setCartItems] = useRecoilState(cartItemsState);
+
+	const deleteItem = () => {
+		const setItems = cartItems.filter(cartItem => cartItem.id !== item.id);
+		setCartItems(setItems);
+	};
+
 	return (
 		<li className="cart-item" key={item.id}>
-			<div className="item-image">
-				<img src={item.image} alt={item.title} />
-			</div>
-
 			<div className="align-box">
-				<div className="item-info">
-					<h4 className="title">{item.title}</h4>
-					<span className="quan">1</span>
-					<span className="option">ONE SIZE</span>
+				<div className="item-image">
+					<img src={item.image} alt={item.title} />
 				</div>
 
-				<button className="delete-button">Remove</button>
+				<div className="item-center-box">
+					<div className="item-info">
+						<h4 className="title">{item.title}</h4>
+						<span className="quan">1</span>
+						<span className="option">ONE SIZE</span>
+					</div>
+					<button className="delete-button" onClick={deleteItem}>
+						Remove
+					</button>
+				</div>
 			</div>
 
 			<div className="item-price">
