f00905520fdfbfacce7cb1452d4f09c60a3fbf3f
Jiyeon <yeeoni_@naver.com>
2022-03-06 17:31:29 +0900

    🚧 Add total:Price that changes depending on the cart-product-list

--- src/routes/cart.tsx
+++ src/routes/cart.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { cartItemsState } from '../atom';
 import CartItem from '../components/cartItem';
 
 const Cart = () => {
-	const cartItems = useRecoilValue(cartItemsState);
+	const [cartItems, setCartItems] = useRecoilState(cartItemsState);
 
 	console.log(cartItems);
+
+	const priceList = cartItems.map(item => {
+		return item.price;
+	});
+
+	const totalPrice = priceList.reduce((a, b) => a + b, 0);
 
 	return (
 		<div className="cart">
@@ -14,15 +20,15 @@ const Cart = () => {
 				<div className="row">
 					<div className="col-sm-4">
 						<ul className="cart-list">
-							{cartItems.map(item => {
-								return <CartItem item={item} key={item.id} />;
-							})}
+							{cartItems.map(item => (
+								<CartItem item={item} key={item.id} />
+							))}
 						</ul>
 
 						<div className="total-price">
 							<dl>
 								<dt>TOTAL</dt>
-								<dd>&#36;230</dd>
+								<dd>&#36;{totalPrice}</dd>
 							</dl>
 						</div>
 					</div>
