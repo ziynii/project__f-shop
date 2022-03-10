b0436e9026a0bbba26312b805f5a85731d452fe2
Jiyeon <yeeoni_@naver.com>
2022-03-11 00:01:24 +0900

    🚧 Implement snsLink component

---
+++ src/components/snsLink.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SnsLink = () => {
+	return (
+		<div className="sns-link">
+			<button className="sns-button">
+				<i className="fa-brands fa-youtube"></i>
+			</button>
+			<button className="sns-button">
+				<i className="fa-brands fa-instagram"></i>
+			</button>
+			<button className="sns-button">
+				<i className="fa-brands fa-facebook"></i>
+			</button>
+		</div>
+	);
+};
+
+export default SnsLink;
