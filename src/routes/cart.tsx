bef36ea8f401b34fb18dcce4c04926cb34644b1a
Jiyeon <yeeoni_@naver.com>
2022-03-16 13:49:24 +0900

    🚧 Implement cart component in tablet+

--- src/routes/cart.tsx
+++ src/routes/cart.tsx
@@ -1,43 +1,54 @@
-import React from 'react';
-import { useRecoilValue } from 'recoil';
-import { cartItemsState } from '../atom';
+import React, { useEffect } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { cartItemsState, goBackState, headerGnbState } from '../atom';
 import CartList from '../components/cartList';
+import PriceBox from '../components/priceBox';
 
 const Cart = () => {
 	const cartItems = useRecoilValue(cartItemsState);
-
+	const setIsGoBack = useSetRecoilState(goBackState);
+	const setHeaderGnb = useSetRecoilState(headerGnbState);
 	const priceList = cartItems.map(item => {
 		return item.price;
 	});
+	const summaryPrice = priceList.reduce((a, b) => a + b, 0);
 
-	const totalPrice = priceList.reduce((a, b) => a + b, 0);
+	useEffect(() => {
+		setIsGoBack(false);
+		setHeaderGnb('');
+	}, []);
 
 	return (
 		<div className="cart">
 			<div className="container">
 				<div className="row">
 					<div className="col-sm-4">
-						{cartItems.length === 0 ? (
-							<div className="cart-empty">
-								<i className="fa-solid fa-cart-shopping"></i> 장바구니가 비어있습니다
+						<div className="align-box">
+							<div className="cart-content">
+								{cartItems.length === 0 ? (
+									<div className="cart-empty">
+										<i className="fa-solid fa-cart-shopping"></i> 장바구니가 비어있습니다
+									</div>
+								) : (
+									<CartList />
+								)}
+								<div className="total-price sm-only">
+									<dl>
+										<dt>TOTAL</dt>
+										<dd>&#36;{summaryPrice}</dd>
+									</dl>
+								</div>
 							</div>
-						) : (
-							<CartList />
-						)}
 
-						<div className="total-price">
-							<dl>
-								<dt>TOTAL</dt>
-								<dd>&#36;{totalPrice}</dd>
-							</dl>
+							<button type="button" className="pay-button sm-only">
+								주문하기
+							</button>
+
+							<PriceBox cartItems={cartItems} summaryPrice={summaryPrice} />
 						</div>
 					</div>
 				</div>
 			</div>
-
-			<button type="button" className="pay-button">
-				주문하기
-			</button>
 		</div>
 	);
 };
