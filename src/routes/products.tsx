9d5ac60fb30f3a6eb3c1343a366961b7e606de93
Jiyeon <yeeoni_@naver.com>
2022-03-16 20:14:58 +0900

    🚧 Add useEffect for headerGnb state

--- src/routes/products.tsx
+++ src/routes/products.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
 import { getCategoryProducts, IProduct } from '../api';
+import { headerGnbState } from '../atom';
 import Loading from '../components/loading';
 import ProductCard from '../components/productCard';
 
 const Category = () => {
 	let { name } = useParams();
-
 	const { data, isLoading } = useQuery<IProduct[]>(
 		['products', (name = name?.includes('men') ? name + "'s clothing" : name)],
 		() => getCategoryProducts(name),
 	);
-
 	const shortName = name?.replace("'s clothing", '');
+	const [headerGnb, setHeaderGnb] = useRecoilState<string | undefined>(headerGnbState);
+
+	useEffect(() => {
+		setHeaderGnb(shortName);
+	}, [name]);
 
 	return (
 		<div className="products">
