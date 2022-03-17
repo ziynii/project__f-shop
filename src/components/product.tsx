6b8399e9464fd33cb57043f5ed19390621c2bd92
Jiyeon <yeeoni_@naver.com>
2022-03-17 18:37:38 +0900

    🎨 Split structure into .product-left/right-section for desktop

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -8,21 +8,25 @@ interface IProductProps {
 const Product = ({ data }: IProductProps) => {
 	return (
 		<div className="product">
-			<div className="product-image">
-				<img src={data?.image} alt={data?.title + '이미지'} />
+			<div className="product-left-section">
+				<div className="product-image">
+					<img src={data?.image} alt={data?.title + '이미지'} />
+				</div>
 			</div>
 
-			<div className="product-category">{data?.category}</div>
+			<div className="product-right-section">
+				<div className="product-category">{data?.category}</div>
 
-			<div className="product-title">
-				<h2>{data?.title}</h2>
+				<div className="product-title">
+					<h2>{data?.title}</h2>
+				</div>
+
+				<div className="product-price">
+					<span>&#36;{data?.price}</span>
+				</div>
+
+				<p className="product-description">{data?.description}</p>
 			</div>
-
-			<div className="product-price">
-				<span>&#36;{data?.price}</span>
-			</div>
-
-			<p className="product-description">{data?.description}</p>
 		</div>
 	);
 };
